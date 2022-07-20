43de961544fcff66e10386985d98d0ea058e1ffb
Beatriz Souza Sartori <102486199+beatrizssartori@users.noreply.github.com>
2022-07-20 15:42:16 -0300

    Add files via upload

---
+++ Eventos/js/scripts.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form");
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const nome = document.querySelector('#nome').value;
+    const email = document.querySelector('#email').value;
+    const telefone = document.querySelector('#phone').value;
+
+    const result = criaCad(nome, email, telefone);
+    const table = document.querySelector('table');
+    table.innerHTML += `
+        <tr>
+            <tr>${result.nome}</td>
+            <td>${result.email}</td>
+            <td>${result.telefone}</td>
+        <tr>
+    `;
+})
+
+//Factory function
+function criaCad(nome, email, telefone) {
+    return {
+        nome: nome,
+        email: email,
+        telefone: telefone
+    }
+}
+
+const atributo = document.querySelector('#atributo');
+atributo.setAttribute("class","atributo");
+console.log(atributo);
