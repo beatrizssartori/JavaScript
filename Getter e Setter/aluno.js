afaac0f041ab4fa5f584ccad34c339650f321c76
Beatriz Souza Sartori <102486199+beatrizssartori@users.noreply.github.com>
2022-07-21 21:38:27 -0300

    Add files via upload

---
+++ Getter e Setter/aluno.js
@@ -0,0 +1,35 @@
+export default class Aluno { //export default = essa classe pode ser usada por qualquer arquivo
+    // Atributo privado
+    #matricula
+    constructor(nome, email, matricula) {
+        this.nome = nome;
+        this.email = email;
+        this.#matricula = matricula;
+    }
+
+    //Getter - Método acessor para retornar dado (para leitura)
+    get matricula() {
+        return this.#matricula
+    }
+
+    //Setter - Método acessor para alterar dados (para escrita)
+    set matricula(novaMatricula) {
+        if (novaMatricula == '') {
+            throw new Error('Por favor, preencha uma matrícula válida')
+        }
+        this.#matricula = novaMatricula
+    }
+
+    exibirInformacoes() {
+        return 'Nome: ${this.nome}, matrícula: ${this.matricula}';
+    }
+}
+
+// const aluno1 = new Aluno('Beatriz', 'bissartori@gmail.com', '20211103');
+// console.log(aluno1.matricula);
+
+// // No terminal Git Bash: Digitar "node aluno.js" para aparecer o console.log
+
+// // Chamando o setter (alterar dado)
+// aluno1.matricula = '20211104'
+// console.log(aluno1.matricula);
---
+++ Getter e Setter/professor.js
@@ -0,0 +1,18 @@
+//Herança
+import Aluno from './aluno.js';
+
+class Professor extends Aluno {
+    constructor(nome,email,matricula) {
+        super(nome,email,matricula)
+    }
+
+    // Polimorfismo
+    exibirInformacoes() {
+        return 'Nome do professor: ${this.nome}';
+    }
+}
+
+// Digitar "npm init -y" no Terminal para abrir o package.json, e adicionar ' "type": "module" '
+
+const professor1 = new Professor('Maurilio Benevento', 'maurilio.benevento@gmail.com', '20030101');
+console.log(professor1.exibirInformacoes());
